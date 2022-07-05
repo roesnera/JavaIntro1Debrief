a04c6f07cad397348edd732158b702a29446810c
JEMIRI DANIEL TAIWO <updatedan2@gmail.com>
2022-07-05 15:53:13 -0500

    Solving CompoundAssignment IncAndDec MathOps & Sout

--- src/com/example/practice/Sout.java
+++ src/com/example/practice/Sout.java
@@ -15,6 +15,14 @@ public class Sout {
         // declare and define a variable of each of the 8 primitive data types in Java
         // (byte, short, int, long, float, double, boolean, char)
 
+        byte myByte = 10;
+        short x = 20000;
+        int myNum = 5;
+        long myLongNum = 12345987609L;
+        float myFloatNum = 8.99f;
+        double myNumber = 5.235d;
+        boolean isEven = true;
+        char myLetter = 'D';
 
 
 
@@ -26,6 +34,14 @@ public class Sout {
         // use System.out.println() to display the values of each variable to the system
 
 
+        System.out.println(myByte);
+        System.out.println(x);
+        System.out.println(myNum);
+        System.out.println(myLongNum);
+        System.out.println(myFloatNum);
+        System.out.println(myNumber);
+        System.out.println(isEven);
+        System.out.println(myLetter);
 
 
 
